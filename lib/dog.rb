a3e863a6e57d8576ffa30fd4f3672e820a3dacc7
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-08-04 16:58:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,3 @@
 class Dog 
   
-  def initialize(dogs_name,breed)
-    @name = dogs_name
-    @breed = breed 
-  end 
   
-  def name (dogs_name)
-    @Fido 
-    end 
-   
-    
-    def breed 
-      pug 
-    end 
-    
-  end 
-    
-    
-
-    
